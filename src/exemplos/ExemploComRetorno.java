0a9f9b0bbe0dc5a42d022026d98f7ceb5b6345cd
kawany-cristina <kawany.cristina07@gmail.com>
2020-03-04 22:07:36 -0300

    commit inicial

---
+++ src/exemplos/ExemploComRetorno.java
@@ -0,0 +1,24 @@
+package exemplos;
+
+public class ExemploComRetorno {
+	
+ public static concatenar (string nome, string sobrenome) {
+	 
+	 return nome + "    "  + sobrenome ;
+ }
+ 
+ public static int tamanhoNomeCompleto (string nome, string sobrenome) {
+	 
+	 return nomeCompleto.light (); 
+ }
+ 
+ main (){
+ 
+	 string nome = concatenar("Kawany", "Cristina");
+	 int tamanho = tamanoNomeCompleto ("kawany", "Cristina");
+	 syso ("Nome" + Nome);
+	 syso ("Qtde:" + tamanho + "caracteres");
+}
+ 
+}
+
